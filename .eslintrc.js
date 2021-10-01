974ea728339205654f01b8e3545174eb2e419c39
cperdiansyah <chandraperdiansyah@gmail.com>
2021-10-01 13:38:08 +0700

    test commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    amd: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y', 'import', 'prettier'],
+  ignorePatterns: ['jsconfig.json', '**/*.test.js'],
+  settings: {
+    'import/resolver': {
+      node: {
+        path: ['src'],
+      },
+    },
+  },
+  rules: {
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    // allow jsx syntax in js files (for next.js project)
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], //should add ".ts" if typescript project
+  },
+};
